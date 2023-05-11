e362d8955f4717eb5e2ebb00eced1277cc9cc109
shivendra_work <shivendra.kumar@piramal.com>
2023-05-11 17:38:23 +0530

    setting up devop and update in displaying dbginfo

---
+++ devop.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+from other_pages.loginpage import login_main
+from other_pages.feed import show_feed
+from other_pages.sadhana_card import sc_main
+from other_pages.departments import structure_main
+from other_pages.settlement import settlement_main
+
+
+st.set_page_config(page_title="BDV-development",
+                    page_icon='📖',
+                    layout='centered'
+                    )
+# #MainMenu {visibility: hidden;}
+hide_menu_style = """
+        <style>
+        # footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+st.session_state['DEBUG_ERROR'] = True
+USER = {"password":"trinadapi","name":"Shivendra","roles":"acc_ic","group":"yud","settlement_id":"32"}
+# ======================================= 
+state_page_map = {'feed':show_feed,
+                  'Sadhana_Card':sc_main,
+                  'dept_structure':structure_main,
+                  'settlement':settlement_main
+                    }
+
+if 'state' not in st.session_state:
+    # default behaviour
+    st.session_state['state'] = 'feed'
+    st.session_state['user'] = USER
+    st.session_state['user']['roles'] = st.session_state['user']['roles'].split(",")
+    
+    def goto(page):
+        st.session_state.state = page
+
+    left,middle,right = st.columns(3)
+    left.button('Sadhana Card',on_click=goto,args=['Sadhana_Card'],key='sc')
+    middle.button('💸 settlement',on_click=goto,args=['settlement'],key='acc')
+    right.button('bdv departments',on_click=goto,args=['dept_structure'],key='dept')
+    
+    left.button('Feed',on_click=goto,args=['feed'],key='feed')
+else :
+    # directed behaviour
+    try:
+        state_page_map[st.session_state.state]()    
+    except Exception as e:
+        if st.session_state['DEBUG_ERROR']: 
+            st.write(e)
+
+
+    
+st.markdown('---')
+st.markdown("[help improve!!](http://wa.me/917260869161?text=Hare%20Krishna%20some%20suggestion)")
+#---
+# if st.session_state
+
+
+
+
+
+
+
+
+
+
+
+
--- other_pages/loginpage.py
+++ other_pages/loginpage.py
@@ -1,14 +1,11 @@
-# Some imports
 import streamlit as st
-import streamlit.components as comp
 # talk to google
-from other_pages.googleapi import upload_data
 from other_pages.googleapi import download_data
 import json
 
 # ------------------------ some Constants
 USER_CREDENTIALS = 'credentials!A2'
-# ------------------------ some Constants end
+# ------------------------ /some Constants
 
 # ======================== some functions
 def change_subpage(subpage):
@@ -23,16 +20,10 @@ def user_login():
             raw_data = download_data(db_id=1,range_name=USER_CREDENTIALS)[0][0]            
             st.session_state['all_user_data'] = json.loads(raw_data)
         except Exception as e:
-            st.write(e)
+            if st.session_state['DEBUG_ERROR']:
+                st.write(e)
             st.error("some problem in fetching data")
-            def rerun():
-                st.session_state['exp_rerun'] = True
-                
-            st.image(image='./other_pages/images/baby_Krishna_Crying.jpg',width=150)
-            st.button("retry",on_click=rerun)
-            st.write(":blue[ensure you are connected to internet]")
-            st.write("please close the tab and open a new one")            
-            return None
+            
     
     all_user_data = st.session_state['all_user_data']
         
@@ -48,7 +39,7 @@ def user_login():
                         key='password')
     
         
-    
+    ## Verify username and password
     if input_user in all_user_data.keys():
         pswd = all_user_data[input_user]['password']
         if input_password == pswd:
@@ -69,136 +60,9 @@ def user_login():
             right.button('bdv departments',on_click=takemein,args=['dept_structure'],key='dept')
     st.markdown("---")
     st.markdown("""<p><a href="http://wa.me/917260869161?text=Hare%20Krishna%20Pr%20I%20forgot%20my%20password%20please%20">
-<img src="https://icon-library.com/images/change-password-icon/change-password-icon-28.jpg" width="75" height="50">
+<img src="https://icon-library.com/images/change-password-icon/change-password-icon-28.jpg" width="90" height="50">
 </a></p>""",unsafe_allow_html=True)
 
-# def registration():
-#     # to register a new user
-#     st.header(":green[Hare Krishna]")
-#     st.subheader(":blue[New User Registration]")
-#     with st.expander("Steps for registratino",expanded=True):
-#         st.markdown("## Steps")
-#         st.markdown("1. Enter a username that has not been used before")
-#         st.markdown("2. After submit, you will need to collect a four digit PIN from respective devotee")
-#         st.markdown(" :blue[With four digit pin you can set the password]")
-        
-#         st.markdown(':green[Then usual login]')
-
-#     st.subheader(':blue[Step 1]')
-#     username = st.text_input("Enter Username")
-#     usercreds = st.secrets['credentials']    
-#     users = fetch_data(usercreds['sheetID'],usercreds['range'])[0][0]
-
-#     usersinfo = json.loads(users)
-#     if username in usersinfo.keys():
-#         st.markdown(':red[Username already Taken!!]')
-#     elif username !="":
-#         st.markdown(':green[Username available]')
-        
-#         def userapply(username):
-#             if 'applied' in st.session_state and username in st.session_state['applied'] :
-#                 st.session_state['resubmitform'] = "# :red[Haribol Pr!!, You have already applied]"
-#             else:
-#                 sheetid = usercreds['sheetID']
-#                 range = 'registration!A:A'
-#                 append_range(sheetid,range,[[username]])
-#                 st.session_state['applied'] = username
-#                 st.session_state['confirm_submission'] =":green[Your request Submitted !!]"
-
-
-#         st.button('Submit',on_click=userapply,args=[username])
-#         if 'resubmitform' in st.session_state:
-#             st.markdown(st.session_state['resubmitform'])
-#             st.session_state.pop('resubmitform')
-#         if 'confirm_submission' in st.session_state:
-#             st.markdown(st.session_state['confirm_submission'])
-#             st.session_state.pop('confirm_submission')
-#     st.markdown('---')
-#     step2 = {'submitted':False}
-    
-#     st.subheader(':blue[Step 2]')
-#     st.caption("With name and pin")
-#     step2['username'] = st.text_input("Enter username",key='user2')
-#     step2['pin'] = st.text_input("Enter pin",key='pin2')
-#     step2['pass1'] = st.text_input("Enter new password",key='pass1')
-#     step2['pass2'] = st.text_input("Confirm password",key='confirm_pass2')
-
-#     status = st.radio("Completed",options=['not','yes'],horizontal=True)
-#     step2['submitted'] = status == 'yes'
-#     if step2['submitted']:
-
-#         if step2['username'] =="":
-#             st.write(":red[enter username]")
-#         else:
-#             usercreds = st.secrets['credentials']
-#             reg_status = fetch_data(usercreds['sheetID'],'credentials!C2')[0][0]
-#             reg_status = json.loads(reg_status)
-#             st.session_state['reguser'] = {'userid':step2['username'],
-#                                             'name':reg_status[step2['username']]['name'],
-#                                             'group':reg_status[step2['username']]['group'],
-#                                             'password':step2['pass1']
-#                                             }
-#             if step2['pin'] =="":
-#                 st.write(":red[enter pin]")
-#             elif step2['pin'] != reg_status[step2['username']]['pin']:
-#                 st.write(step2['pin'])
-#                 st.write(reg_status[step2['username']]['pin'])
-#                 st.write(":red[wrong pin]")
-#             elif step2['pass1'] =="":
-#                 st.write(":red[enter password]")
-#             elif step2['pass1'] != step2['pass2']:
-#                 st.write(":red[password does not match]")
-#             else:
-                
-#                 if step2['username'] in reg_status.keys():
-#                     if step2['pin'] == reg_status[step2['username']]['pin']:
-#                         # show the submit button
-#                         def First_Login():
-
-#                             reginfo = st.session_state['reguser']
-#                             creds = st.secrets['credentials']
-#                             db_accounts = fetch_data_forced(creds['sheetID'],creds['range'])[0][0]
-#                             db_accounts = json.loads(db_accounts)
-#                             if step2['username'] in db_accounts.keys():
-#                                 st.write(":green[Your account created already]")
-#                                 st.caption("please contact for moreinfo")
-#                             else:
-#                                 new_db_accounts = {**db_accounts,
-#                                                     **{reginfo['userid']:{'name':reginfo['name'],
-#                                                                                         'password':reginfo['password'],
-#                                                                                         'group':reginfo['group']}
-#                                                         }
-#                                                     }
-#                                 new_db_accounts = json.dumps(new_db_accounts,sort_keys=True)
-#                                 response = update_range(creds['sheetID'],
-#                                                         creds['range'],
-#                                                         [[new_db_accounts]])
-#                                 verify = json.loads(response['values'][0][0])
-#                                 if step2['username'] in verify.keys():
-#                                     st.session_state['reg_response'] = True
-#                                 else:
-#                                     st.session_state['reg_response'] = False
-#                             # st.write(db_accounts)
-#                             # st.write(new_db_accounts)
-
-
-#                         st.button("Create Account",on_click=First_Login)
-#                         if 'reg_response' not in st.session_state:
-#                             pass
-#                         elif st.session_state['reg_response']:
-#                             st.write(":green[Account created successfully!!]")
-#                             def gotomain():
-#                                 st.session_state['substate'] = 'default'
-#                                 # update userinfo
-#                                 usercreds = st.secrets['credentials']
-#                                 users = fetch_data_forced(usercreds['sheetID'],usercreds['range'])[0][0]    
-#                                 usersinfo = json.loads(users)
-#                                 st.session_state['usersinfo'] = usersinfo
-#                             st.button("Go to login",on_click=gotomain)
-#                         else:
-#                             st.write(":red[Some api error]")
-#                 else:
-#                     st.write(":red[invalid username]")
 
 # ---------------------- Wrapper
 view_dict= {'default':user_login}
