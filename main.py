a5941d4912ef51a133ed70d515ff4ef0bc1ad2f8
shivendra_work <shivendra.kumar@piramal.com>
2023-06-20 17:10:24 +0530

    created_template and main runner

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-from other_pages.loginpage import login_main
-from other_pages.feed import show_feed
-from other_pages.sadhana_card import sc_main
-from other_pages.departments import structure_main
-from other_pages.settlement import settlement_main
-from other_pages.japa_talk import jt_main
-from other_pages.article_collection import get_article_main
+# from other_pages.loginpage import login_main
+# from other_pages.feed import show_feed
+# from other_pages.sadhana_card import sc_main
+# from other_pages.departments import structure_main
+# from other_pages.settlement import settlement_main
+# from other_pages.japa_talk import jt_main
+# from other_pages.article_collection import get_article_main
 
 
 if 'LAYOUT' not in st.session_state:
@@ -19,39 +19,31 @@ st.set_page_config(page_title="BDV",
                     page_icon='📖',
                     layout=st.session_state.LAYOUT
                     )
-# #MainMenu {visibility: hidden;}
-hide_menu_style = """
+st.markdown( """
         <style>
+        # #MainMenu {visibility: hidden;}
         # footer {visibility: hidden;}
         </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+        """, unsafe_allow_html=True)
+
 st.session_state['DEBUG_ERROR'] = True
 
 # ======================================= 
-state_page_map = {'feed':show_feed,
-                  'Sadhana_Card':sc_main,
-                  'dept_structure':structure_main,
-                  'settlement':settlement_main,
-                  'japa_talk':jt_main,
-                  'article_collection': get_article_main
-                    }
+if 'status_navigator' not in st.session_state:
+    # for the first run create the instances of all the available pages
+    # This will not run next since the condition will be false
 
-if 'state' not in st.session_state:
-    # default behaviour
-    login_main()
+    st.session_state['status_navigator'] = {
+        'login':None,
+        'feed': None, # create instance of the feed class (after importing)
+        'settlement': None
+    }
+    # Set the landing page to login
+    st.session_state['_page'] = 'login'
 
-else :
-    # directed behaviour
-    try:
-        state_page_map[st.session_state.state]()    
-    except Exception as e:
-        if st.session_state['DEBUG_ERROR']:
-            st.write(e)       
+# run the root loop
+st.session_state['status_navigator'][st.session_state['_page']].run()
 
-
-
-    
 st.markdown('---')
 st.markdown("[help improve!!](http://wa.me/917260869161?text=Hare%20Krishna%20some%20suggestion)")
 
--- other_pages/template.py
+++ other_pages/template.py
@@ -1,33 +1,17 @@
-import streamlit as st
-import json
 
-from other_pages.googleapi import download_data
-from other_pages.googleapi import upload_data
-
-# ============= some variables
-# ============= some variables end
+from typing import Any
 
 
-## ----------- call back functions
-def change_page(state,substate='default'):
-    st.session_state['state'] = state
-    st.session_state['substate'] = substate
+class template_page:
+    def __init__(self) -> None:
+        
+        self.subpage = 'home'
+        self.subpage_navigator = {
+            'home':self.home
+        }
 
-def change_subpage(subpage):
-    st.session_state['substate'] = subpage
-
-## -------------
-# ---------------------- Wrapper
-pagestate_map = {}
-
-def somename_main():
-    if 'substate' not in st.session_state:
-        # default behaviour
+    def home(self):
         pass
 
-    elif st.session_state['substate'] in pagestate_map.keys():
-        # directed behaviour
-        pagestate_map[st.session_state['substate']]()
-    else:
-        # exceptional
-        pass 
+    def run(self):
+        self.subpage_navigator[self.subpage]()
