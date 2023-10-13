dbc9bb1704f4e43fe15d2f8408f528674162b0cf
shivendra_work <shivendra.kumar@piramal.com>
2023-10-13 12:19:56 +0530

    dfgj

--- other_pages/find_transcript.py
+++ other_pages/find_transcript.py
@@ -1,13 +1,27 @@
 import streamlit as st
 
 
-class lec_finder_Class:
-    def __init__(self) -> None:
+class finder_Class:
+    def __init__(self):
         
-        self.page_dict = {}
-        self.current_page = 
+        self.page_dict = {'home':self.home,
+                          'sp_transcript':self.vedabase_SP,
+                          'idt_hhrnsm':self.idt_lectures_by_HHRNSM}
+        self.current_page = 'home'
     
     @property
     def bdvapp(self):
         return st.session_state.get('bdv_app',None)
     
+    def home(self):
+        pass
+
+    def vedabase_SP(self):
+        pass
+
+    def idt_lectures_by_HHRNSM(self):
+        pass
+    
+
+    def run(self):
+        self.page_dict[self.current_page]()
